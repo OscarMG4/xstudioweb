34e6869a8683d472a373011465d1d57d5ae3e33b
Oscar Fernando Muñoz Gayoso <86798585+OscarMG4@users.noreply.github.com>
2024-10-28 15:37:52 -0500

    transiciones Imagenes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,20 @@ import { faCameraRetro } from '@fortawesome/free-solid-svg-icons';
 
 export const Hero = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-      <div className="text-center lg:text-start space-y-6 font-cityBurn">
+    <section className="container flex flex-col lg:flex-row items-center justify-center py-20 md:py-32">
+      <div className="text-center lg:text-start space-y-6 font-cityBurn lg:mr-10 mb-10 lg:mb-0">
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">"Desata El Poder De Tu Marca Con</h1>{" "}
           <span className="inline bg-gradient-to-r from-[#beb8b6] via-[#4b4d4f] to-[#666d72] text-transparent bg-clip-text">
             STUDIO X"
           </span>{" "}
         </main>
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+        <p
+          className="text-xl text-white md:w-10/12 mx-auto lg:mx-0"
+          style={{
+            textShadow: '2px 2px 4px black, -2px -2px 4px black, 2px -2px 4px black, -2px 2px 4px black',
+          }}
+        >
           Dale vida a tu visión. Studio X convierte ideas en imágenes que hablan,
           inspiran y generan compromiso
         </p>
@@ -22,14 +27,14 @@ export const Hero = () => {
             rel="noreferrer noopener"
             href="https://github.com/leoMirandaa/shadcn-landing-page.git"
             target="_blank"
-            className={`w-full md:w-1/3 ${buttonVariants({ variant: "outline" })} portfolio-link`} // Añade la clase aquí
+            className={`w-full md:w-1/3 ${buttonVariants({ variant: "outline" })} portfolio-link`}
           >
-            <FontAwesomeIcon icon={faCameraRetro} className="h-5 w-5 inline-block mr-2" /> {/* Ícono */}
+            <FontAwesomeIcon icon={faCameraRetro} className="h-5 w-5 inline-block mr-2" />
             Portafolio
           </a>
         </div>
       </div>
-      <div className="z-10">
+      <div className="z-10 w-full lg:w-1/2">
         <HeroCards />
       </div>
       <div className="shadow"></div>
--- src/components/SobreNosotros.tsx
+++ src/components/SobreNosotros.tsx
@@ -1,11 +1,11 @@
-import { Statistics } from "./Statistics";
+
 import pilot from "../assets/pilot.png";
 
 export const SobreNosotros = () => {
   return (
     <section
       id="sobreNosotros"
-className="container flex items-center justify-center min-h-screen py-24 sm:py-32"
+      className="container flex items-center justify-center min-h-screen py-24 sm:py-32"
     >
       <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
@@ -17,21 +17,23 @@ className="container flex items-center justify-center min-h-screen py-24 sm:py-3
           <div className="flex flex-col justify-between">
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
-                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
-                </span>
-                Company
+                Misión
               </h2>
               <p className="text-xl text-muted-foreground mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
+                Nuestra misión es potenciar la presencia digital de marcas y negocios a través de estrategias de marketing innovadoras, producciones audiovisuales de alta calidad y un enfoque en el engagement genuino. Nos dedicamos a crear experiencias visuales que no solo reflejan la esencia de cada cliente, sino que también impulsan el crecimiento y la lealtad en sus comunidades digitales.
               </p>
             </div>
 
-            <Statistics />
+            <div className="pb-6">
+              <h2 className="text-3xl md:text-4xl font-bold">
+                Visión
+              </h2>
+              <p className="text-xl text-muted-foreground mt-4">
+                Ser la agencia líder en producción de contenido y marketing digital en Perú, reconocida por su creatividad, excelencia y capacidad de generar conexiones auténticas entre marcas y audiencias. Aspiramos a redefinir la forma en que las marcas se comunican, inspirando relaciones de valor y posicionando a nuestros clientes como referentes en sus industrias.
+
+
+              </p>
+            </div>
           </div>
         </div>
       </div>
